423e4366d4c1c3b7a6dc428b2ebd13e1efb2f5e6
MoChink <moxipei@outlook.com>
2016-04-12 18:26:08 +0800

    zzz

---
+++ Algorithms/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithms
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("hello algorithms!");
+			int i = 0;
+        }
+    }
+}
